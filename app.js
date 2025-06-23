8ccd3a6488d464b69e05e0006e4fd0a61fab0079
TheoThomas97 <theothomas97@gmail.com>
2025-06-23 11:26:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -23,11 +23,12 @@ toggleButton.addEventListener("click", handlePulse)
 const citiesSelect = document.getElementById("cities");
 const citySpan = document.getElementById("city");
 
-citiesSelect.addEventListener("change", function () {
-  citySpan.textContent = citiesSelect.value;
-});
+function handleSelect(){
+  const newCity = citiesSelect.value
+  citySapn.textContent = newCity
+}
 
-
+citiesSelect.addEventListener("change", handleSelect)
 
 
 //  ? Task 3
@@ -39,22 +40,26 @@ citiesSelect.addEventListener("change", function () {
 const nameDivs = document.querySelectorAll(".name");
 
 // Select the span with id "current"
-const currentSpan = document.getElementById("current");
+const currentEl = document.querySelectorAll("#current");
 
+function handleEnter(event){
+  const nameEl = event.target
+  const selectedName = nameEl.innerHTML
+  currentEl.textcontent = selectedName
+}
+function handleLeave(event){
+  console.log(event.target)
+}
 // Add event listeners to each "name" div
-nameDivs.forEach((div) => {
-  // Part One: When mouse enters, show name
-  div.addEventListener("mouseenter", () => {
-    currentSpan.textContent = div.textContent;
-  });
+nameDivs.forEach(nameEl => {
+  nameEl.addEventListener("mouseover", () => {
+    currentEl.textContent = selectedName
+  })
+  nameEl.addEventListener("mouseover" , handleLeave)
+  nameEl.addEventListner("mouseleave" , handleEnter0
+})
 
-  // Part Two: When mouse leaves, clear the span
-  div.addEventListener("mouseleave", () => {
-    currentSpan.textContent = "";
-  });
-});
-
-
+  
 
 // ? Task 4
 // Add a "submit" event listener to the form with the id of "subscribe"
@@ -63,30 +68,29 @@ nameDivs.forEach((div) => {
 // * Hint 1: If you have added the class correctly, you should see that the button has an opacity of 0.8. 
 // * Hint 2: You will need to prevent the default behaviour of the form in order to stop the page from refreshing before the changes are made.
 
-const form = document.getElementById("subscribe");
+const subscribeForm = document.getElementById("subscribe")
+const submitButton = document.querySelector(".submit")
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault(); // Stop page from refreshing
+function handleSubmit(event){
+  event.preventDefault()
+  submitButton.classList.add("submitted")
+  submitButton.textContent = "Submitted!"
+}
 
-  const button = form.querySelector("button");
-  button.classList.add("submitted");
-  button.innerHTML = "Submitted!";
-});
-
-
+subscribeForm.addEventListener("submit", handleSubmit)
 
 // ? BONUS Task 5
 // Add an event listener to the window, so that when the user scrolls, you check whether or not the scrollY property of the window is greater than 40. 
 // If it is greater than 40, add the class "fadeOutUp" to the <h1> tag. 
 // If it is less than 40, remove the class "fadeOutUp" from the <h1> tag. 
 // * Hint: As you scroll, you should see the <h1> tag fade out. If you scroll back down, it will reappear.
+const h1 = document.querySelector("h1")
 
-const heading = document.querySelector("h1");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 40) {
-    heading.classList.add("fadeOutUp");
-  } else {
-    heading.classList.remove("fadeOutUp");
+function handleScroll(event){
+ const scrollPos = window.scrollY
+  if (scrollPos >= 40) {
+    h1.classList.remove("fadeOutUp")
   }
-});
+}
+
+window.addEventListener("scroll", handlescroll)
